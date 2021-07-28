a8020492782f081f303c3bd3eba5562bdf77178b
Tony <anthonychung14@gmail.com>
2021-07-28 08:57:21 -0700

    Add deps and networks

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+
+// NOTE: THIS IS AN EMPTY ACCOUNT. IT HAS SOME ETH IN RINKEBY
+const PRIVATE_KEY = process.env.ETH_KEY;
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    }
+  }
+};
